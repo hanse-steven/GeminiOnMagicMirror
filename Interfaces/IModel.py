8914b5cfeddf8715953084987136d2f758419da8
Steven Hanse <steven.hanse@live.be>
2025-09-14 02:39:15 +0200

    :art: add intial prompt

--- Interfaces/IModel.py
+++ Interfaces/IModel.py
@@ -1,9 +1,15 @@
 from abc import abstractmethod
 
-from Interfaces.IModelResponse import IResponse
+from Interfaces.IModelResponse import IModelResponse
 
 
 class IModel:
+    first_prompt: str | None = None
+
+    def __init__(self, initial: str | None = None):
+        if initial is not None:
+            first_prompt = initial
+
     @abstractmethod
-    def prompt(self, prompt: str) -> IResponse:
+    def prompt(self, prompt: str) -> IModelResponse:
         pass
